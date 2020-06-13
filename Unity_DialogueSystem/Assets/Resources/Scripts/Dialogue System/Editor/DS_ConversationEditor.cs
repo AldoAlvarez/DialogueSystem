95cc02a0ba78c97cd6ba34d50c01363eb79926d1
AldoAlvarez <alvrz.aldo@gmail.com>
2020-06-12 19:49:12 -0500

    Conversation Custom Editor

---
+++ Unity_DialogueSystem/Assets/Resources/Scripts/Dialogue System/Editor/DS_ConversationEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using AGAC.DialogueSystem.Base;
+
+namespace AGAC.DialogueSystem.CustomEditors
+{
+    [CustomEditor(typeof(DS_Conversation))]
+    public class DS_ConversationEditor : Editor
+    {
+        public void OnEnable()
+        {
+            DS_ConversationInspector.Open();
+        }
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}
